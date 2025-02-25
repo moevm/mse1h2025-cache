c14007dcb585941b8972c6aac15b52b0845a6342
Пахомов Степан Дмитриевич <112636677+vRedniy-LumbeRt@users.noreply.github.com>
2025-02-25 20:43:42 +0300

    Corrections_try_Delete stack/Pytest_overview directory
    Попробовал удалить директорию, возможно, из-за файлов питона проверка не проходит

--- stack/Pytest_overview/tests_dir/test_n1.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def add(a: int, b: int) -> int:
-    return a + b
-
-
-def test_1():
-    assert add(1, 1) == 2
--- stack/Pytest_overview/tests_dir/test_n2.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def mult(a: int, b: int) -> int:
-    return a * b
-
-
-def test_1():
-    assert mult(2, 3) == 6
